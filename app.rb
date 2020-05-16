eff4917131e60aa1f2750136ff55b0e67ff5ff89
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-05-16 02:11:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,7 +14,8 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do 
     @phrase = params[:phrase]
     @num = params[:number].to_i 
-    @number.times(@phrase)
+    @number.times{ str += "#{@phrase}\n" }
+    str
   end 
   
   get '/:operation/:number1' do 

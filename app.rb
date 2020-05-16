1761ba927e61240ef651ea70057bc7e113b2baf6
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-05-16 02:36:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -32,20 +32,19 @@ class App < Sinatra::Base
      "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
    end 
    
-  get '/:operation/:number1/:number2' do 
-    @operation = params[:operation]
+   get '/:operation/:number1/:number2' do
+    @oper = params[:operation]
     @num1 = params[:number1].to_i
-    @num2 = params[:number 2].to_i
-    
-    case @operation
-      when  "add" 
-        (@num1 + @num2).to_s
-      when "subtract" 
+    @num2 = params[:number2].to_i
+    case @oper
+      when "subtract"
         (@num1 - @num2).to_s
-      when "multiply" 
+      when "add"
+        (@num1 + @num2).to_s
+      when "multiply"
         (@num1 * @num2).to_s
-      when "divide" 
+      when "divide"
         (@num1 / @num2).to_s
     end
-  end 
-end 
+  end
+end

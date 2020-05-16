75b35a0856ae30b07607ad17508c992f9463af7e
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-05-16 02:08:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,4 +10,12 @@ class App < Sinatra::Base
     @square = params[:number].to_i
     (@square**2).to_s
   end 
+  
+  get '/say/:number/:phraser' do 
+    @phrase = params[:phraser]
+    @num = params[:number].to_i 
+    @phrase.
+  
+  get '/:operation/:number1' do 
+    
 end

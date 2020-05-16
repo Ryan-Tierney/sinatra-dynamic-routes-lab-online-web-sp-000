d6e3a23f4b531c66af1547098e2087199cdecad2
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-05-16 02:12:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/reversename/:name' do 
-    @user_name = params[:name] 
-    @user_name.reverse 
+    @user_name = params[:name].reverse
+    @user_name
   end 
   
   get '/square/:number' do 
